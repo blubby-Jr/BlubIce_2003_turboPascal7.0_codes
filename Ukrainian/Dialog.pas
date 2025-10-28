20c0e93705d2a1683eb9416ba15cfd7e5da40905
Blub Ice <stendof2.cs@gmail.com>
2025-10-28 22:05:35 +0200

    Add files via upload

---
+++ Ukrainian/Dialog.pas
@@ -0,0 +1,35 @@
+{
+
+                            Online Pascal Compiler.
+                Code, Compile, Run and Debug Pascal program online.
+Write your code in this editor and press "Run" button to execute it.
+
+}
+
+{
+program Hello;
+begin
+  writeln ('Hello World')
+end.
+}
+{
+    пишу код на turbo pascal 7.0 2003 года написания моей племяненницой?
+}
+program Dialog;
+var
+  S, S1, S2: string;
+begin
+  writeln('Добрий День!');
+  writeln('Як вас звати?');
+  readln(S);
+  writeln('Мене звати k4 і мені 103 роки.');
+  writeln(S, ', цікаво, і що ти любиш читати?');
+  readln(S1);
+  writeln('Я люблю читати будь-що.');
+  writeln('А що ти робиш чи чим займаєшся у вільний час?');
+  readln(S2);
+  writeln(S2, ', яка твоя улюблена пора року? І чому?');
+  writeln('Я поспішаю. Приємно було поспілкуватися. До зустрічі!');
+  readln;  { <-- щоб програма не закрилась одразу }
+end.
+
